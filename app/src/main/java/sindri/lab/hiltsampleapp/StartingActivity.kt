0f269a574126c085521ca67ae135a20b2646582b
RajeshKumar <rajeshincorp@gmail.com>
2024-05-15 15:13:08 +0530

    Inject at Activity

--- app/src/main/java/sindri/lab/hiltsampleapp/StartingActivity.kt
+++ app/src/main/java/sindri/lab/hiltsampleapp/StartingActivity.kt
@@ -1,12 +1,20 @@
 package sindri.lab.hiltsampleapp
 
 import android.os.Bundle
+import androidx.activity.ComponentActivity
 import androidx.activity.enableEdgeToEdge
 import androidx.appcompat.app.AppCompatActivity
 import androidx.core.view.ViewCompat
 import androidx.core.view.WindowInsetsCompat
+import dagger.hilt.android.AndroidEntryPoint
+import javax.inject.Inject
 
-class StartingActivity : AppCompatActivity() {
+@AndroidEntryPoint
+class StartingActivity : ComponentActivity() {
+
+    @Inject
+    lateinit var userRepository: UserRepository
+
     override fun onCreate(savedInstanceState: Bundle?) {
         super.onCreate(savedInstanceState)
         enableEdgeToEdge()
@@ -16,5 +24,7 @@ class StartingActivity : AppCompatActivity() {
             v.setPadding(systemBars.left, systemBars.top, systemBars.right, systemBars.bottom)
             insets
         }
+
+        userRepository.saveUser("rajesh@gmail.com","1234")
     }
 }
--- app/src/main/java/sindri/lab/hiltsampleapp/UserApplication.kt
+++ app/src/main/java/sindri/lab/hiltsampleapp/UserApplication.kt
@@ -7,12 +7,8 @@ import javax.inject.Inject
 @HiltAndroidApp
 class UserApplication : Application() {
 
-    @Inject
-    lateinit var userRepository: UserRepository
-
     override fun onCreate() {
         super.onCreate()
-        userRepository.saveUser("rajesh@gmail.com", "12345678")
     }
 
 }
